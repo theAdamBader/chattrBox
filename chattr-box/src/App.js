c783b38894b9d928f2db03bc91495b375d66f76a
Adam Bader <abade011@gold.ac.uk>
2021-07-29 11:11:20 +0100

    added firebase config

--- chattr-box/src/App.js
+++ chattr-box/src/App.js
@@ -9,7 +9,13 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  //your config
+    apiKey: "AIzaSyAdJzL2pS9TMSNuxih6KuVcrblF0RpO3tY",
+    authDomain: "chattr-box.firebaseapp.com",
+    projectId: "chattr-box",
+    storageBucket: "chattr-box.appspot.com",
+    messagingSenderId: "1098584024908",
+    appId: "1:1098584024908:web:3b42ece072cf5a1e8551e9",
+    measurementId: "G-X5KBZWY5MJ"
 })
 
 const auth = firebase.auth();
